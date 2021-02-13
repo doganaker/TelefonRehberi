6a705026d2728c96804c81e92715deb94d193361
Doğan Aker <m.doganaker@gmail.com>
2021-02-13 14:40:18 +0300

    Kisi and ILetisimBilgisi tables created and migrated to database.

---
+++ Rehber/Migrations/20210213113848_DBInit.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Rehber.API.Migrations
+{
+    public partial class DBInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kisis",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Surname = table.Column<string>(type: "text", nullable: true),
+                    Company = table.Column<string>(type: "text", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kisis", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IletisimBilgisis",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Phone = table.Column<string>(type: "text", nullable: true),
+                    EMail = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    KisiID = table.Column<int>(type: "integer", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IletisimBilgisis", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_IletisimBilgisis_Kisis_KisiID",
+                        column: x => x.KisiID,
+                        principalTable: "Kisis",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IletisimBilgisis_KisiID",
+                table: "IletisimBilgisis",
+                column: "KisiID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IletisimBilgisis");
+
+            migrationBuilder.DropTable(
+                name: "Kisis");
+        }
+    }
+}
