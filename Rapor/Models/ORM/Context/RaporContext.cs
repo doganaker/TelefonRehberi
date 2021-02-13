3df82c04d68ff14b9fb899d839ad334066b9cf0a
Doğan Aker <m.doganaker@gmail.com>
2021-02-13 15:18:48 +0300

    Report table constructed

---
+++ Rapor/Models/ORM/Context/RaporContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rapor.API.Models.ORM.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rapor.API.Models.ORM.Context
+{
+    public class RaporContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Server=ec2-54-72-155-238.eu-west-1.compute.amazonaws.com;Database=d1n46ggrpqs40j;UID=phdnzzcnbhxvsf;PWD=eff706218e8f855ccf89cdf6cdc679ca57112c28f630becdadef8849cda67b6b;SSL Mode= Require;TrustServerCertificate=True");
+        }
+
+        public DbSet<Report> Reports{ get; set; }
+    }
+}
---
+++ Rapor/Models/ORM/Entities/Report.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rapor.API.Models.ORM.Entities
+{
+    public class Report
+    {
+        public int ID { get; set; }
+
+        private DateTime _adddate = DateTime.Now;
+        public DateTime AddDate
+        {
+            get
+            {
+                return _adddate;
+            }
+            set
+            {
+                _adddate = value;
+            }
+        }
+
+        private bool _isdeleted = false;
+        public bool IsDeleted
+        {
+            get
+            {
+                return _isdeleted;
+            }
+            set
+            {
+                _isdeleted = value;
+            }
+        }
+
+        private bool _status = false;
+        public bool ReportStatus
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                _status = value;
+            }
+        }
+        public string Address { get; set; }
+        public int TotalKisi { get; set; }
+        public int TotalPhone { get; set; }
+    }
+}
